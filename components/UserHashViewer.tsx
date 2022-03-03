02275653499cbb9d59b152faefe999316378709c
Avestura <me@avestura.dev>
2022-03-04 00:52:34 +0330

    temporarily disable user hash requests

--- components/UserHashViewer.tsx
+++ components/UserHashViewer.tsx
@@ -1,8 +1,9 @@
 import useSWR from "swr"
 
 export const UserHashViewer = (props: {}) => {
-  const { data, error } = useSWR('https://forums.trgwii.com/api/self/hash')
-
+  // const { data, error } = useSWR('https://forums.trgwii.com/api/self/hash')
+  const data = undefined;
+  const error = "error"
   if (error) return <i>failed to load</i>
   if (!data) return <i>loading...</i>
   return <>{JSON.stringify(data)}</>
