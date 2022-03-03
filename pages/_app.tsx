e8d9648e72b026f6b22b914397c0cae6bfb9ef48
Avestura <me@avestura.dev>
2022-03-03 22:13:37 +0330

    add progress for page change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,21 @@ import { useState } from "react";
 import { useColorScheme, useLocalStorageValue } from "@mantine/hooks";
 import { NotificationsProvider } from "@mantine/notifications";
 
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+})
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 process.env.FORUM = "https://forums.trgwii.com";
 
 export default function App(props: AppProps) {
